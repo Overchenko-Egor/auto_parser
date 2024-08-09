69915cdbdfc923bd51b0c41549a0dbe81203a5c8
Overchenko-Egor <egoverchenko@mail.ru>
2024-08-09 14:02:53 +0700

    Изменения, которые будут включены в коммит: 	изменено:      Credits.py 	изменено:      auto_parser/auto_parser/__pycache__/__init__.cpython-310.pyc 	изменено:      auto_parser/auto_parser/__pycache__/settings.cpython-310.pyc 	изменено:      auto_parser/auto_parser/__pycache__/urls.cpython-310.pyc 	изменено:      auto_parser/auto_parser/__pycache__/wsgi.cpython-310.pyc 	изменено:      auto_parser/auto_parser/settings.py 	новый файл:    auto_parser/auto_parser/templates/index.html 	изменено:      auto_parser/auto_parser/urls.py 	изменено:      auto_parser/db.sqlite3 	новый файл:    auto_parser/parser/__init__.py 	новый файл:    auto_parser/parser/__pycache__/__init__.cpython-310.pyc 	новый файл:    auto_parser/parser/__pycache__/admin.cpython-310.pyc 	новый файл:    auto_parser/parser/__pycache__/apps.cpython-310.pyc 	новый файл:    auto_parser/parser/__pycache__/models.cpython-310.pyc 	новый файл:    auto_parser/parser/__pycache__/views.cpython-310.pyc 	новый файл:    auto_parser/parser/admin.py 	новый файл:    auto_parser/parser/apps.py 	новый файл:    auto_parser/parser/migrations/__init__.py 	новый файл:    auto_parser/parser/migrations/__pycache__/__init__.cpython-310.pyc 	новый файл:    auto_parser/parser/models.py 	новый файл:    auto_parser/parser/tests.py 	новый файл:    auto_parser/parser/views.py 	изменено:      main.py

---
+++ Credits.py
@@ -0,0 +1,6 @@
+class cred:
+ 
+    def __init__(login, password):
+        self.login = login
+        self.password = password
+ 
---
+++ auto_parser/parser/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def index_page (requesr):
+    return render(request= 'index.html')
