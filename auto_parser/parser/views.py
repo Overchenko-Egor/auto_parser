454b8adf29ff3d24ae870cd4757e7e867e9818dc
Overchenko-Egor <egoverchenko@mail.ru>
2024-08-12 16:44:35 +0700

    Изменения, которые будут включены в коммит: 	изменено:      auto_parser/auto_parser/__pycache__/settings.cpython-310.pyc 	изменено:      auto_parser/auto_parser/settings.py 	изменено:      auto_parser/parser/__pycache__/models.cpython-310.pyc 	изменено:      auto_parser/parser/__pycache__/views.cpython-310.pyc 	переименовано: auto_parser/auto_parser/templates/index.html -> auto_parser/parser/templates/index.html 	изменено:      auto_parser/parser/views.py

--- auto_parser/parser/views.py
+++ auto_parser/parser/views.py
@@ -1,4 +1,8 @@
+from urllib import request
 from django.shortcuts import render
+from django.http import HttpRequest
 
-def index_page (requesr):
-    return render(request= 'index.html')
+
+def index_page (req):
+    return render(req, 'index.html')
+
